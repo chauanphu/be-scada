49d83b443161f600758397732932207fd5901207
Phu Chau <chauanphu@gmail.com>
2024-10-18 23:02:30 +0700

    bug: allow CORs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+
 from app import create_app
 import uvicorn
 from websocket_manager import websocket_endpoint
@@ -9,6 +11,19 @@ app = create_app()
 @app.websocket("/ws/unit/{unit_id}/status")
 async def websocket_route(websocket: WebSocket, unit_id: int):
     await websocket_endpoint(websocket, unit_id)
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    # Add more origins as needed
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
